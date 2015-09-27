250f86e9d43e228ffbbd88f7bdc6f3d7d1178a68
AlphaBetaCourseraDelta <AlphaBetaCourseraDelta@users.noreply.github.com>
2015-09-27 02:55:34 -0700

    update
    added documentation to the UI

--- ForTheGold/ui.R
+++ ForTheGold/ui.R
@@ -5,18 +5,25 @@ shinyUI(fluidPage(
   
   # Application title
   titlePanel("Olympic Medal Count"),
+
   
-  # Sidebar with a slider input for the number of bins
+  # Sidebar- create 3 inputs, one for year, one for sport and one for medal type
+  
   sidebarLayout(
     sidebarPanel(
+      p("The graph on the right shows the number of gold, silver, and/or bronze medals won by each country in a particular sport over a particular time period"),
+      p("Use the controls below to choose what years you'd like to see, which sports you're interested in (pick 'ALL' to see all sports), and whether you'd like to look at the distribution of gold, silver, bronze, or all medals "),
+      p("The data is from",
+      a(em("The Guardian's"),"datasets.", href="http://www.theguardian.com/sport/datablog/2012/jun/25/olympic-medal-winner-list-data#data"),
+      "It includes medal winner data from 1896 to 2008."),
       sliderInput("year", label = "Years",
                   min=min(base$Edition), max=max(base$Edition), step=4, value=c(1992,1996)),
       selectInput("sport", label="Sport",
                   choices=c("ALL",as.character(unique(base$Sport))),
                   selected="Aquatics"),
       radioButtons("color", label="Medal",
-                  choices=c("ALL","Gold","Silver","Bronze"),
-                  selected="ALL")
+                   choices=c("ALL","Gold","Silver","Bronze"),
+                   selected="ALL")
     ),
     
     # Show a plot of the generated distribution
